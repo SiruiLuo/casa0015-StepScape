d343912414046206125ce6c33b22e92bc804e3ad
SiruiLuo <115336377+SiruiLuo@users.noreply.github.com>
2025-04-22 00:13:39 +0100

    Data protection

--- android/app/src/main/kotlin/com/example/stepscape/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.stepscape
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/config.dart
+++
@@ -1,4 +0,0 @@
-class Config {
-  static const String googleMapsApiKey = 'AIzaSyAMw8_JlFSx1EzaEJvpAdc0hd2lVc9CX3c';
-  static const String googleAirQualityApiKey = 'AIzaSyAMw8_JlFSx1EzaEJvpAdc0hd2lVc9CX3c';
-} 
